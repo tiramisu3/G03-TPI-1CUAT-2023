9f2dda331342771c055328b1573cb2fd0ca1ddde
catalinaperinetti <129747762+catalinaperinetti@users.noreply.github.com>
2023-08-02 10:00:40 -0300

    Anda la tabla que muestra laspalabras

--- index.js
+++ index.js
@@ -76,15 +76,16 @@ app.post('/login', async function(req, res)
 app.post('/Admin', async function(req, res){
     console.log("Soy un pedido POST", req.query);
     res.render('Admin', null);
+    /*
     console.log("Soy un pedido PUT", req.body); //En req.body vamos a obtener el objeto con los parámetros enviados desde el frontend por método PUT
     let vector = [await MySQL.realizarQuery(` SELECT * FROM Palabras `)]
     console.log(vector)
     if (vector.length > 0) {
-        res.send({palabra: true})    
+        res.send({palabra: vector})    
     }
     else{
         res.send({palabra:false})    
-    }
+    }*/
 });
 
 app.put('/login', async function(req, res) {
@@ -106,10 +107,10 @@ app.put('/Admin', async function(req, res) {
     let vector = [await MySQL.realizarQuery(` SELECT * FROM Palabras `)]
     console.log(vector)
     if (vector.length > 0) {
-        res.send({palabra: true})    
+        res.send({palabras: vector})    
     }
     else{
-        res.send({palabra:false})    
+        res.send({palabras:false})    
     }
 });
 
@@ -150,3 +151,5 @@ app.post('/nuevousuario', async function(req, res)
 app.post('/home', async function(req, res){
     
 })
+
+app.delete()
