65de1a32f2ede02d4eefcbd836951f74572401aa
FaustoAmiano <famiano@pioix.edu.ar>
2023-08-13 19:12:31 -0300

    Update BASE DE DATOS PROYECTO.sql

--- BASE DE DATOS PROYECTO.sql
+++ BASE DE DATOS PROYECTO.sql
@@ -3,21 +3,21 @@ CREATE TABLE Usuarios (
     mail varchar(100) primary key,
     nom_usuario varchar(30),
     contraseña varchar(30),
-    esadmin boolean
-    
+    esadmin boolean,
+    puntaje int
 );
 
-INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin)
-Values ( "famiano@gmail.com", "famiano", "433", true);
+INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin, puntaje)
+Values ( "famiano@gmail.com", "famiano", "433", true, 0);
 ;
-INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin)
-Values ( "Ichu@gmail.com", "Ichu", "899", false);
+INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin, puntaje)
+Values ( "Ichu@gmail.com", "Ichu", "899", false, 0);
 ;
-INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin)
-Values ( "Pico@gmail.com", "Matip", "8552", false);
+INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin, puntaje)
+Values ( "Pico@gmail.com", "Matip", "8552", false, 0);
 ;
-INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin)
-Values ( "MartinRivas@gmail.com", "Rivas", "963", true);
+INSERT INTO Usuarios (mail, nom_usuario, contraseña, esadmin, puntaje)
+Values ( "MartinRivas@gmail.com", "Rivas", "963", true, 0);
 ;
 select * 
 From Usuarios
